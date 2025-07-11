bf2ae504c542645180ba3042b950ae8eee006c31
Programmering <teknisk+programmering-GDC2025-GR7@game.unf.dk>
2025-07-11 21:52:37 +0200

    movement alpha stage færdig

--- MainUnityProject/Assets/PlayerMover.cs
+++ MainUnityProject/Assets/PlayerMover.cs
@@ -9,27 +9,26 @@ public class PlayerMover : MonoBehaviour
     public float speed = 1f;
     Rigidbody rb;
     public InputAction moveAction;
+    public InputAction jumpAction;
     public float gravity = 1f;
-    public BoxCollider BoxCollider;
     bool grounded = true;
+    public float jumpForce = 10f;
+    public float fastFall = 5f;
+    public float fallSpeed = 10f;
     void Start()
     {
         moveAction.Enable();
+        jumpAction.Enable();
 
         rb = GetComponent<Rigidbody>();
     }
 
     void OnTriggerEnter(Collider other)
     {
-
-
+        print("called");
         if (other.gameObject.CompareTag("Ground"))
         {
             grounded = true;
-        }
-        else
-        {
-            
         }
         
     }
@@ -38,23 +37,41 @@ public class PlayerMover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-    
+        print(grounded);
+        
         float moveInput = moveAction.ReadValue<float>();
 
         Vector3 newVelocity = rb.linearVelocity;
-        
-        if (grounded == true)
-        {
-            
-        }
-        else
-        {
-            
-        }
 
         newVelocity.x = moveInput * speed;
         rb.linearVelocity = newVelocity;
 
         
+        
+        float jumpInput = jumpAction.ReadValue<float>();
+
+        print(jumpInput);
+        
+        if (grounded == true && jumpAction.WasPressedThisFrame())
+        {
+            rb.AddForce(Vector3.up * jumpForce);
+            grounded = false;
+            
+        }
+        else if (grounded == false && jumpInput < 0)
+        {
+            rb.AddForce(Vector3.down * fastFall);
+            
+            
+        }
+        
+        // Negative speed increases after reaching max height of the jump
+        
+        if (newVelocity.y < 0)
+        {
+            rb.AddForce(Vector3.down * fallSpeed);
+        }
+
+
     }
 }
